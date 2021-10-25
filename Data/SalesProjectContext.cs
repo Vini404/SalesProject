d15492df093a12ef82ea8828ae50cc531387df66
vini404 <vinicius404contato@gmail.com>
2021-10-25 16:05:18 -0300

    Models Complete and schemas is ready

--- Data/SalesProjectContext.cs
+++ Data/SalesProjectContext.cs
@@ -15,5 +15,10 @@ namespace SalesProject.Data
         }
 
         public DbSet<Department> Department { get; set; }
+        public DbSet<Seller> Seller { get; set; }
+
+        public DbSet<SalesRecord> SalesRecord { get; set; }
+
+
     }
 }
