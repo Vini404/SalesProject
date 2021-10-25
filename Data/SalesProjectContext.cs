d725a7725f24325fdf689e50376be4df8581c199
vini404 <vinicius404contato@gmail.com>
2021-10-25 14:34:28 -0300

    Controller/View/Model is ready

---
+++ Data/SalesProjectContext.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SalesProject.Models;
+
+namespace SalesProject.Data
+{
+    public class SalesProjectContext : DbContext
+    {
+        public SalesProjectContext (DbContextOptions<SalesProjectContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Department> Department { get; set; }
+    }
+}
